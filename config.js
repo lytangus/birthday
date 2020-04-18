1b35f9d4afa43e7edceb2059d554c2395d2996cd
lytangus <641985483@qq.com>
2020-04-18 12:36:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "心爱的猪猪",  // 同上...
         "今天是你的生日",
         "这是我们在一起的",
         "第三个生日了哦",
         "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
+        "是我陪你一起过的",
+        "今年要在爷爷加要多吃点哦",
         "要把我家可爱猪猪喂饱饱",
         "然后抱走",
         "YAMI~~",
@@ -25,17 +25,17 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "我的猪猪，快快长大，早日出栏": "./imgs/猪猪.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        turn_on: "猪猪点我",
+        play: "猪猪再点我",
+        bannar_coming: "继续",
+        balloons_flying: "不要停",
+        cake_fadein: "该吃蛋糕了",
+        light_candle: "点蜡烛许愿吧",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
